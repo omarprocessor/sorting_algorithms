ec80e20ae988e7b512590a0c620a61f244d90617
omarprocessor <wadaniinarais1@gmail.com>
2024-10-28 21:37:20 -0700

    task done

--- sort.h
+++ sort.h
@@ -23,4 +23,5 @@ void bubble_sort(int *array, size_t size);
 void print_number(int n);
 void print_char(char c);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 #endif /* SORT_H */
